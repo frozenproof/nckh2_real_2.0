afa37cf660828e7ed19fc886ead7e1c9be70d1f4
frozenproof <trucling222@gmail.com>
2023-02-14 18:35:45 +0700

    Updated a lot , and added base script for minting

--- pages/api/create-minting-transaction.ts
+++ pages/api/create-minting-transaction.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { AppWallet, KoiosProvider,ForgeScript, AssetMetadata } from '@meshsdk/core';
 
-type Data = {
-  name: string
-}
+
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res: NextApiResponse) {
+
+    const koiosProvider = new KoiosProvider('<api|preview|preprod|guild>');
+
+    
+    const wallet = new AppWallet({
+      networkId: 0,
+      fetcher: koiosProvider,
+      submitter: koiosProvider,
+      key: {
+        type: 'mnemonic',
+        words: ["solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution"],
+      },
+    });
+
+    const AppWalletAddress = wallet.getPaymentAddress();
+    const forgingScript = ForgeScript.withOneSignature(AppWalletAddress);
+
+    const assetName = 'DemonMeoToken';
+      
+    const assetMetadata: AssetMetadata = {
+      "name": "Demon Token",
+      "image": "ipfs://QmRzicpReutwCkM6aotuKjErFCUD213DpwPq6ByuzMJaua",
+      "mediaType": "image/jpg",
+      "description": "This NFT is minted by Mesh (https://meshjs.dev/)."
+    };
+  res.status(200).json({})
 }
