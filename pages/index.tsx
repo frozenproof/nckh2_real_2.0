bd040207b921cfe37186d5236b8043cb2c721161
frozenproof <trucling222@gmail.com>
2023-02-14 17:27:53 +0700

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import { CardanoWallet } from '@meshsdk/react';
+import { CardanoWallet,useWallet  } from '@meshsdk/react';
 
 export default function Home() {
+  const { wallet,connected, disconnect, error } = useWallet();
+  async function test(){
+    // console.log("Test is good");
+    const changeAddress = await wallet.getChangeAddress();
+    console.log("Here is the Change address",changeAddress);
+    
+    const utxos = await wallet.getUtxos();
+    console.log("Here is the utxos",utxos);
+    
+  }
   return (
     <>
      <CardanoWallet />
+     <button onClick={()=>test()}>Test</button>
     </>
   )
 }
