297ab577fab13a33e8dc84c4661293236d120537
frozenproof <trucling222@gmail.com>
2023-02-14 20:21:13 +0700

    Added and repaired broken functions

--- pages/api/create-minting-transaction.ts
+++ pages/api/create-minting-transaction.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { AppWallet, KoiosProvider,ForgeScript, AssetMetadata } from '@meshsdk/core';
+import { AppWallet, KoiosProvider, ForgeScript, AssetMetadata, Mint } from '@meshsdk/core';
 
 
 
@@ -8,29 +8,38 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse) {
 
-    const koiosProvider = new KoiosProvider('<api|preview|preprod|guild>');
-    const recipentAddress=req.body.recipentAddress;
-    
-    const wallet = new AppWallet({
-      networkId: 0,
-      fetcher: koiosProvider,
-      submitter: koiosProvider,
-      key: {
-        type: 'mnemonic',
-        words: ["solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution","solution"],
-      },
-    });
+  const koiosProvider = new KoiosProvider('<api|preview|preprod|guild>');
+  const recipentAddress = req.body.recipentAddress;
+  const utxos = req.body.utxos;
 
-    const AppWalletAddress = wallet.getPaymentAddress();
-    const forgingScript = ForgeScript.withOneSignature(AppWalletAddress);
+  const wallet = new AppWallet({
+    networkId: 0,
+    fetcher: koiosProvider,
+    submitter: koiosProvider,
+    key: {
+      type: 'mnemonic',
+      words: ["solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution", "solution"],
+    },
+  });
 
-    const assetName = 'DemonMeoToken';
-      
-    const assetMetadata: AssetMetadata = {
-      "name": "Demon Token",
-      "image": "ipfs://QmRzicpReutwCkM6aotuKjErFCUD213DpwPq6ByuzMJaua",
-      "mediaType": "image/jpg",
-      "description": "This NFT is minted by Mesh (https://meshjs.dev/)."
-    };
+  const AppWalletAddress = wallet.getPaymentAddress();
+  const forgingScript = ForgeScript.withOneSignature(AppWalletAddress);
+
+  const assetName = 'DemonMeoToken';
+
+  const assetMetadata: AssetMetadata = {
+    "name": "Demon Token",
+    "image": "ipfs://QmZhn8oALDSpDv4MQjsFtafYCk35moAwE1cQmw7EpGTTTV",
+    "mediaType": "image/png",
+    "description": "This NFT is minted by Create NFT."
+  };
+
+  const asset: Mint = {
+    assetName: assetName,
+    assetQuantity: '1',
+    metadata: assetMetadata,
+    label: '721',
+    recipient: recipentAddress,
+  };
   res.status(200).json({})
 }
