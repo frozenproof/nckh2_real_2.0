88e7db431f99f3d2fed7f6d8a2c7d3f1eaba010e
frozenproof <trucling222@gmail.com>
2023-02-14 16:41:45 +0700

    Installed new meshjs and added new config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
+};
+module.exports = nextConfig;
