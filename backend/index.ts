94cfffa3b3b3dd4bb8b36d0083cf590a3f4913e4
frozenproof <trucling222@gmail.com>
2023-02-14 23:04:59 +0700

    temporary fix

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,7 @@
 import axios from "axios"
+
+//create a new instance of axios, from url api because there is folder api inside pages folder
+const instance = axios.create({
+    baseURL:'/api/',
+    withCredentials: true,
+});
