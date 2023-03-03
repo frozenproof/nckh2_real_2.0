b8c276c157bcddf58859365d617237a4c6f0cc5d
frozenproof <trucling222@gmail.com>
2023-03-03 23:39:14 +0700

    Last seen running

--- pages/createTransaction.ts
+++ pages/createTransaction.ts
@@ -30,27 +30,9 @@ export async function sendingDataTobackend(
     try {
         // return await post(`dynamic-minting`, { recipentAddress, utxos });
         // return await post(`multi-dynamic-minting`, { recipentAddress, utxos });
-        var test_names = ["hi3.png", "l6209640.png", "uyu.jpg"];
-        var imageAddress = ["QmTWjgDhu57zV2UTG9a2FkWVkemNKmYKEnhmtFVFv2FryH", "QmZhn8oALDSpDv4MQjsFtafYCk35moAwE1cQmw7EpGTTTV", "QmPfgm9PYZPpLD6muD5WStEjYNWLYR29xrqUbsrSuTCuEt"];
-        var test_types = ["png", "png", "jpg"];
-        // let buffer = fs.readFileSync(`${__dirname}/../server/log/CutRes.log`);
-        // let fileContent = buffer.toString();
-        // let result = fileContent.split("\n");
-        // var test_names = [,];
-        // var imageAddress = [,];
-        // var test_types = [,];
-
-        // for (var i = 0; i < result.length; i = i + 3) {
-        //     var a = i;
-        //     test_names.push(result[a]);
-        //     test_types.push(result[a + 1]);
-        //     imageAddress.push(result[a + 2]);
-
-        //     console.log(result[a]);
-        // }
-
         
-        return await post(`automate-minting`, { recipentAddress, utxos, test_names, imageAddress, test_types });
+        return await post(`minting`, { recipentAddress, utxos});
+        // return await post(`automate-minting`, { recipentAddress, utxos, test_names, imageAddress, test_types });
     }
     catch (err: unknown) {
         console.log(err); //Object is of type 'unknown'
