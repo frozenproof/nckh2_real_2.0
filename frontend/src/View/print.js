9ddcc0574ad4667b63715fdcdbfb4955e357f4ae
frozenproof <trucling222@gmail.com>
2023-03-03 20:35:38 +0700

    Overhauling

---
+++ frontend/src/View/print.js
@@ -0,0 +1,3 @@
+function alertme() {
+    alert("Hello! I am an alert box!");
+  }
--- server/testServer.js
+++ server/testServer.js
@@ -9,13 +9,16 @@ app.use(express.json());
 
 // Cho phép lý dữ liệu từ form method POST
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("amodule"));
-
+// app.use(express.static("amodule"));
+app.use(express.static(__dirname + '/amodule'));
 // Khởi tạo các routes cho ứng dụng
 initRoutes(app);
 
 // chọn một port mà bạn muốn và sử dụng để chạy ứng dụng tại local
 let port = 3001;
+app.get('/', function(req, res) {
+  // do something here.
+});
 
 app.listen(port, () => {
   console.log(`Hello meo con, I'm running at localhost:${port}\n`);
