f8ad18116862ee5544001bc620b59442cb730793
frozenproof <trucling222@gmail.com>
2023-03-04 01:14:21 +0700

    Completed integration

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 let getHome = (req, res) => {
-  // return res.sendFile(path.join(`${__dirname}/../../frontend/src/View/homepage.html`));
+  return res.sendFile(path.join(`${__dirname}/../../frontend/src/View/homepage.html`));
   // return res.sendFile(path.join(`${__dirname}/../../frontend/src/View/after.html`));
-  return res.sendFile(path.join(`${__dirname}/../../out/index.html`));
+  // return res.sendFile(path.join(`${__dirname}/../../out/index.html`));
 };
 
 module.exports = {
